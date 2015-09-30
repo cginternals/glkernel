36053b8e2d632da08d656dc9af808ce19ad6ed56
Simon Krogmann <simon.krogmann@student.hpi.de>
2015-09-30 15:48:37 +0200

    Add best candidate sampling

--- source/glkernel/include/glkernel/sample.h
+++ source/glkernel/include/glkernel/sample.h
@@ -22,6 +22,19 @@ size_t poisson_square(tkernel<glm::tvec2<T, P>> & kernel, unsigned int num_probe
 template <typename T, glm::precision P>
 size_t poisson_square(tkernel<glm::tvec2<T, P>> & kernel, T min_dist, unsigned int num_probes = 32);
 
+/*
+*  @brief
+*  Generates several candidates for each samples and selects the one
+*  that is farthest to the previously selected samples
+*
+*  @param[in,out] kernel
+*  The kernel to be modified
+*
+*  @param[in] num_candidates
+*  Number of candidates generated for each sample
+*/
+template <typename T, glm::precision P>
+void best_candidate(tkernel<glm::tvec2<T, P>> & kernel, unsigned int num_candidates = 32);
 
 } // namespace sample
 
