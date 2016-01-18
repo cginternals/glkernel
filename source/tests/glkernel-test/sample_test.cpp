ab948b854cf59c7afa0f513c82d8a709e4c04328
Daniel Limberger <daniel.limberger@hpi.de>
2016-01-18 10:17:19 +0100

    merge

--- source/tests/glkernel-test/sample_test.cpp
+++ source/tests/glkernel-test/sample_test.cpp
@@ -82,3 +82,52 @@ TEST_F(sample_test, stratified_one_sample_per_stratum)
     }
     ASSERT_EQ(1, count);
 }
+
+TEST_F(sample_test, n_rooks_compile)
+{
+    auto fkernel2 = glkernel::kernel2{ 1 };
+
+    glkernel::sample::n_rooks(fkernel2);
+
+    auto dkernel2 = glkernel::dkernel2{ 1 };
+
+    glkernel::sample::n_rooks(dkernel2);
+}
+
+TEST_F(sample_test, n_rooks_one_sample_per_column)
+{
+    auto fkernel2 = glkernel::kernel2{ 20 };
+
+    glkernel::sample::n_rooks(fkernel2);
+
+    // test if first column contains one sample
+    unsigned int count = 0;
+    for (size_t i = 0; i < fkernel2.size(); ++i)
+    {
+        auto &sample = fkernel2[i];
+        if (0.f <= sample.x && sample.x < 1.f / 20.f)
+        {
+            ++count;
+        }
+    }
+    ASSERT_EQ(1, count);
+}
+
+TEST_F(sample_test, n_rooks_one_sample_per_row)
+{
+    auto fkernel2 = glkernel::kernel2{ 20 };
+
+    glkernel::sample::n_rooks(fkernel2);
+
+    // test if first row contains one sample
+    unsigned int count = 0;
+    for (size_t i = 0; i < fkernel2.size(); ++i)
+    {
+        auto &sample = fkernel2[i];
+        if (0.f <= sample.y && sample.y < 1.f / 20.f)
+        {
+            ++count;
+        }
+    }
+    ASSERT_EQ(1, count);
+}
