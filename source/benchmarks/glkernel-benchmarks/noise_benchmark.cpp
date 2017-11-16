2c6138d82f26fc41a223c999a833355b45f60f89
Florian Wagner <florian.bonn@gmail.com>
2017-11-16 15:31:08 +0100

    A first basic benchmark

---
+++ source/benchmarks/glkernel-benchmarks/noise_benchmark.cpp
@@ -0,0 +1,44 @@
+
+#include <benchmark/benchmark.h>
+
+#include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
+#include <glm/vec4.hpp>
+
+#include <glkernel/Kernel.h>
+#include <glkernel/noise.h>
+
+static void BM_gradientNoise_linear(benchmark::State& state) {
+    auto dkernel = glkernel::dkernel1{state.range(0)};
+
+
+    for (auto _ : state)
+        glkernel::noise::gradient(dkernel, glkernel::noise::GradientNoiseType::Perlin);
+
+}
+
+BENCHMARK(BM_gradientNoise_linear)->RangeMultiplier(2)->Range(2, 64);
+
+static void BM_gradientNoise_quadratic(benchmark::State& state) {
+    auto dkernel = glkernel::dkernel1{state.range(0), state.range(0), state.range(0)};
+
+
+    for (auto _ : state)
+        glkernel::noise::gradient(dkernel, glkernel::noise::GradientNoiseType::Perlin);
+
+}
+
+BENCHMARK(BM_gradientNoise_quadratic)->RangeMultiplier(2)->Range(2, 64)->Complexity(benchmark::oNSquared);
+
+static void BM_gradientNoise_cube(benchmark::State& state) {
+    auto dkernel = glkernel::dkernel1{state.range(0), state.range(0), state.range(0)};
+
+
+    for (auto _ : state)
+        glkernel::noise::gradient(dkernel, glkernel::noise::GradientNoiseType::Perlin);
+
+}
+
+BENCHMARK(BM_gradientNoise_cube)->RangeMultiplier(2)->Range(2, 64)->Complexity(benchmark::oNCubed);
+
+BENCHMARK_MAIN();
