e3b339141e1a171fd49d4f7e9c54b6db97a17034
Jonas Beyer <jonas.beyer@student.hpi.de>
2017-11-20 17:19:18 +0100

    Add basic command line flag handling using cppassist/cmdline

--- source/tools/glkernel-cli/main.cpp
+++ source/tools/glkernel-cli/main.cpp
@@ -1,5 +1,28 @@
+#include <iostream>
+#include <string>
+
+#include <cppassist/cmdline/ArgumentParser.h>
+
 
 int main(int argc, char* argv[])
 {
+  cppassist::ArgumentParser argParser;
+  argParser.parse(argc, argv);
+
+  auto inFileName = argParser.value("--input");
+  auto outFileName = argParser.value("--output");
+
+  auto outFileType = outFileName.substr(outFileName.find_last_of('.') + 1);
+
+  auto fromGeneratedKernel = argParser.isSet("-k");
+
+  if (fromGeneratedKernel)
+  {
+    std::cout << "Converting kernel \"" << inFileName << "\" to output file \"" << outFileName << "\" (type: " << outFileType << ")" <<std::endl;
+  }
+  else
+  {
+    std::cout << "Using kernel description \"" << inFileName << "\" to generate kernel \"" << outFileName << "\" (type: " << outFileType << ")" << std::endl;
+  }
 
 }
