0905f9faacc460b6539e65d699a61d5d603a3381
Jonas Beyer <jonas.beyer@student.hpi.de>
2017-12-04 15:18:20 +0100

    Use switch instead of different actions

--- source/tools/glkernel-cli/main.cpp
+++ source/tools/glkernel-cli/main.cpp
@@ -8,8 +8,8 @@
 #include <cppassist/cmdline/ArgumentParser.h>
 #include <cppassist/cmdline/CommandLineProgram.h>
 #include <cppassist/cmdline/CommandLineAction.h>
-#include <cppassist/cmdline/CommandLineCommand.h>
 #include <cppassist/cmdline/CommandLineOption.h>
+#include <cppassist/cmdline/CommandLineSwitch.h>
 
 
 int main(int argc, char* argv[])
@@ -20,69 +20,72 @@ int main(int argc, char* argv[])
         "A command line interface for generating and converting kernels."};
 
 
-    auto actionGenerate = cppassist::CommandLineAction{"generate", "Generate a kernel from a kernel description"};
-    auto actionConvert = cppassist::CommandLineAction{"convert", "Convert an existing kernel into another representation"};
+    auto actionRun = cppassist::CommandLineAction{"run",
+        "Generate a kernel from a kernel description or convert an existing kernel into another representation"};
 
-    auto commandGenerate = cppassist::CommandLineCommand{"generate"};
-    auto commandConvert = cppassist::CommandLineCommand{"convert"};
+    auto optInputFile = cppassist::CommandLineOption{
+        "--input",
+        "-i",
+        "inFileName",
+        "Kernel description or generated kernel",
+        cppassist::CommandLineOption::NonOptional};
 
-    auto optInputFile = cppassist::CommandLineOption{"--input", "-i", "inFileName",
-        "Kernel description or generated kernel", cppassist::CommandLineOption::NonOptional};
-    auto optOutputFile = cppassist::CommandLineOption{"--output", "-o", "outFileName",
-        "File that the generated / converted kernel will be written to", cppassist::CommandLineOption::NonOptional};
-    auto optOutputFormat = cppassist::CommandLineOption{"--format", "-f", "outFileFormat",
-        "File format for the generated / converted kernel (e.g. json, png)", cppassist::CommandLineOption::Optional};
+    auto optOutputFile = cppassist::CommandLineOption{
+        "--output",
+        "-o",
+        "outFileName",
+        "File that the generated / converted kernel will be written to",
+        cppassist::CommandLineOption::NonOptional};
 
-    actionGenerate.add(&commandGenerate);
-    actionGenerate.add(&optInputFile);
-    actionGenerate.add(&optOutputFile);
-    actionGenerate.add(&optOutputFormat);
+    auto optOutputFormat = cppassist::CommandLineOption{
+        "--format",
+        "-f",
+        "outFileFormat",
+        "File format for the generated / converted kernel (e.g. json, png)",
+        cppassist::CommandLineOption::Optional};
 
-    actionConvert.add(&commandConvert);
-    actionConvert.add(&optInputFile);
-    actionConvert.add(&optOutputFile);
-    actionConvert.add(&optOutputFormat);
+    auto swConvert = cppassist::CommandLineSwitch{
+        "--convert",
+        "-c",
+        "Convert an existing kernel into another representation instead of generating a new one",
+        cppassist::CommandLineSwitch::Optional};
 
-    program.add(&actionGenerate);
-    program.add(&actionConvert);
+
+    actionRun.add(&optInputFile);
+    actionRun.add(&optOutputFile);
+    actionRun.add(&optOutputFormat);
+    actionRun.add(&swConvert);
+
+    program.add(&actionRun);
 
     program.parse(argc, argv);
 
     if (program.selectedAction() && !program.hasErrors()) {
 
         // Generate kernel from description
-        if (program.selectedAction() == &actionGenerate) {
-            const auto& inFileName = optInputFile.value();
-            const auto& outFileName = optOutputFile.value();
-            auto outFileFormat = optOutputFormat.value();
-            if (outFileFormat.empty()) {
-                if (outFileName.find('.') == std::string::npos) {
-                    outFileFormat = "json";
-                } else {
-                    outFileFormat = outFileName.substr(outFileName.find_last_of('.') + 1);
-                }
-            }
-            cppassist::info() << "Using kernel description \"" << inFileName << "\" to generate kernel \"" << outFileName << "\" (format: " << outFileFormat << ")";
-        }
-
-        // Convert kernel to other representation
-        else if (program.selectedAction() == &actionConvert) {
-            const auto& inFileName = optInputFile.value();
-            const auto& outFileName = optOutputFile.value();
-            auto outFileFormat = optOutputFormat.value();
-            if (outFileFormat.empty()) {
+        const auto &inFileName = optInputFile.value();
+        const auto &outFileName = optOutputFile.value();
+        auto outFileFormat = optOutputFormat.value();
+        if (outFileFormat.empty()) {
+            if (outFileName.find('.') == std::string::npos) {
+                outFileFormat = "json";
+            } else {
                 outFileFormat = outFileName.substr(outFileName.find_last_of('.') + 1);
-                if (outFileName.find('.') == std::string::npos) {
-                    outFileFormat = "json";
-                } else {
-                    outFileFormat = outFileName.substr(outFileName.find_last_of('.') + 1);
-                }
             }
-            cppassist::info() << "Converting kernel \"" << inFileName << "\" to output file \"" << outFileName << "\" (format: " << outFileFormat << ")";
         }
 
-        // Success
+        if (swConvert.activated()) {
+            // Convert kernel to other representation
+            cppassist::info() << "Converting kernel \"" << inFileName << "\" to output file \"" << outFileName
+                              << "\" (format: " << outFileFormat << ")";
+        } else {
+            // Generate kernel from description
+            cppassist::info() << "Using kernel description \"" << inFileName << "\" to generate kernel \""
+                              << outFileName << "\" (format: " << outFileFormat << ")";
+        }
+
         return 0;
+        // Success
     }
 
     else {
