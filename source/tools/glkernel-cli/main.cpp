5eed0a2151fb0707ae4391ca07a5ae275f567b72
Jonas Beyer <jonas.beyer@student.hpi.de>
2017-12-04 14:31:21 +0100

    Rewrite CLI using cppassist::CommandLineProgram

--- source/tools/glkernel-cli/main.cpp
+++ source/tools/glkernel-cli/main.cpp
@@ -1,28 +1,103 @@
 #include <iostream>
 #include <string>
 
+#include <cppassist/logging/logging.h>
+
 #include <cppassist/cmdline/ArgumentParser.h>
+#include <cppassist/cmdline/CommandLineProgram.h>
+#include <cppassist/cmdline/CommandLineAction.h>
+#include <cppassist/cmdline/CommandLineCommand.h>
+#include <cppassist/cmdline/CommandLineOption.h>
+#include <cppassist/cmdline/CommandLineSwitch.h>
+#include <cppassist/cmdline/CommandLineParameter.h>
 
 
 int main(int argc, char* argv[])
 {
-    cppassist::ArgumentParser argParser;
-    argParser.parse(argc, argv);
+    auto program = cppassist::CommandLineProgram{
+        "glkernel-cli",
+        "glkernel-cli", // TODO version, license?
+        "A command line interface for generating and converting kernels."};
 
-    auto inFileName = argParser.value("--input");
-    auto outFileName = argParser.value("--output");
 
-    auto outFileType = outFileName.substr(outFileName.find_last_of('.') + 1);
+    auto actionGenerate = cppassist::CommandLineAction{"generate", "Generate a kernel from a kernel description"};
+    auto actionConvert = cppassist::CommandLineAction{"convert", "Convert an existing kernel into another representation"};
 
-    auto fromGeneratedKernel = argParser.isSet("-k");
+    auto commandGenerate = cppassist::CommandLineCommand{"generate"};
+    auto commandConvert = cppassist::CommandLineCommand{"convert"};
 
-    if (fromGeneratedKernel)
-    {
-        std::cout << "Converting kernel \"" << inFileName << "\" to output file \"" << outFileName << "\" (type: " << outFileType << ")" <<std::endl;
+    auto optInputFile = cppassist::CommandLineOption{"--input", "-i", "inFileName",
+        "Kernel description or generated kernel", cppassist::CommandLineOption::NonOptional};
+    auto optOutputFile = cppassist::CommandLineOption{"--output", "-o", "outFileName",
+        "File that the generated / converted kernel will be written to", cppassist::CommandLineOption::NonOptional};
+    auto optOutputFormat = cppassist::CommandLineOption{"--format", "-f", "outFileFormat",
+        "File format for the generated / converted kernel (e.g. json, png)", cppassist::CommandLineOption::Optional};
+
+    actionGenerate.add(&commandGenerate);
+    actionGenerate.add(&optInputFile);
+    actionGenerate.add(&optOutputFile);
+    actionGenerate.add(&optOutputFormat);
+
+    actionConvert.add(&commandConvert);
+    actionConvert.add(&optInputFile);
+    actionConvert.add(&optOutputFile);
+    actionConvert.add(&optOutputFormat);
+
+    program.add(&actionGenerate);
+    program.add(&actionConvert);
+
+    program.parse(argc, argv);
+
+    if (program.selectedAction() && !program.hasErrors()) {
+
+        // Generate kernel from description
+        if (program.selectedAction() == &actionGenerate) {
+            const auto& inFileName = optInputFile.value();
+            const auto& outFileName = optOutputFile.value();
+            auto outFileFormat = optOutputFormat.value();
+            if (outFileFormat.empty()) {
+                if (outFileName.find('.') == std::string::npos) {
+                    outFileFormat = "json";
+                } else {
+                    outFileFormat = outFileName.substr(outFileName.find_last_of('.') + 1);
+                }
+            }
+            cppassist::info() << "Using kernel description \"" << inFileName << "\" to generate kernel \"" << outFileName << "\" (format: " << outFileFormat << ")";
+        }
+
+        // Convert kernel to other representation
+        else if (program.selectedAction() == &actionConvert) {
+            const auto& inFileName = optInputFile.value();
+            const auto& outFileName = optOutputFile.value();
+            auto outFileFormat = optOutputFormat.value();
+            if (outFileFormat.empty()) {
+                outFileFormat = outFileName.substr(outFileName.find_last_of('.') + 1);
+                if (outFileName.find('.') == std::string::npos) {
+                    outFileFormat = "json";
+                } else {
+                    outFileFormat = outFileName.substr(outFileName.find_last_of('.') + 1);
+                }
+            }
+            cppassist::info() << "Converting kernel \"" << inFileName << "\" to output file \"" << outFileName << "\" (format: " << outFileFormat << ")";
+        }
+
+        // Success
+        return 0;
     }
-    else
-    {
-        std::cout << "Using kernel description \"" << inFileName << "\" to generate kernel \"" << outFileName << "\" (type: " << outFileType << ")" << std::endl;
+
+    else {
+        // Print help
+        program.print(program.help(program.selectedAction()));
+
+        // Print errors
+        if (program.hasErrors() && program.selectedAction())
+        {
+            std::string error = program.selectedAction()->errors()[0];
+            program.print("Error: " + error);
+
+            return 1;
+        }
     }
 
+    return 0;
 }
