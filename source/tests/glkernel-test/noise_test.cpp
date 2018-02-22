65da23953ce734fc4cd0c31ad71b9bd89345cfc2
Florian Wagner <florian.bonn@gmail.com>
2018-02-22 16:04:27 +0100

    cont.

--- source/tests/glkernel-test/noise_test.cpp
+++ source/tests/glkernel-test/noise_test.cpp
@@ -18,18 +18,18 @@ class noise_test: public testing::Test
 TEST_F(noise_test, normal_compile)
 {
     auto fkernel1 = glkernel::kernel1{ 1 };
-    auto fkernel3 = glkernel::kernel3{ 1 };
     auto fkernel2 = glkernel::kernel2{ 1 };
+    auto fkernel3 = glkernel::kernel3{ 1 };
     auto fkernel4 = glkernel::kernel4{ 1 };
 
     glkernel::noise::normal(fkernel1, 0.f, 1.f);
-    glkernel::noise::normal(fkernel3, 0.f, 1.f);
     glkernel::noise::normal(fkernel2, 0.f, 1.f);
+    glkernel::noise::normal(fkernel3, 0.f, 1.f);
     glkernel::noise::normal(fkernel4, 0.f, 1.f);
 
-    glkernel::noise::normal(fkernel2, glm::vec2{ 0.f }, glm::vec2{ 1.f });
-    glkernel::noise::normal(fkernel3, glm::vec3{ 0.f }, glm::vec3{ 1.f });
-    glkernel::noise::normal(fkernel4, glm::vec4{ 0.f }, glm::vec4{ 1.f });
+    glkernel::noise::normal(fkernel2, glm::vec2( 0.f ), glm::vec2( 1.f ));
+    glkernel::noise::normal(fkernel3, glm::vec3( 0.f ), glm::vec3( 1.f ));
+    glkernel::noise::normal(fkernel4, glm::vec4( 0.f ), glm::vec4( 1.f ));
 
     auto dkernel1 = glkernel::dkernel1{ 1 };
     auto dkernel2 = glkernel::dkernel2{ 1 };
